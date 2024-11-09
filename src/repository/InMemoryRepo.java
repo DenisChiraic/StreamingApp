3b26cbd96ac0b4bb3fe241c5a2ab1aae4f195d20
Denis <denis.chiriac@stud.ubbcluj.ro>
2024-11-09 12:29:29 +0200

    Added InMemoryRepo

--- src/repository/InMemoryRepo.java
+++ src/repository/InMemoryRepo.java
@@ -1,4 +1,34 @@
 package repository;
 
-public class InMemoryRepo {
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class InMemoryRepo<T> implements IRepo<T> {
+    private final Map<UUID, T> storage = new ConcurrentHashMap<>();
+
+    @Override
+    public void create(T obj) {
+        storage.put(UUID.randomUUID(), obj);
+    }
+
+    @Override
+    public Optional<T> read(UUID id) {
+        return Optional.ofNullable(storage.get(id));
+    }
+
+    @Override
+    public void update(T obj) {
+        storage.put(UUID.randomUUID(), obj);
+    }
+
+    @Override
+    public void delete(UUID id) {
+        storage.remove(id);
+    }
+
+    @Override
+    public List<T> findAll() {
+        return new ArrayList<>(storage.values());
+    }
+
 }

cbea7d76ac6aa8fb975750be4402cdffef3c6528
14ShaDoW141 <149473066+14ShaDoW141@users.noreply.github.com>
2024-11-09 12:12:46 +0200

    Added IRepo

--- src/repository/IRepo.java
+++ src/repository/IRepo.java
@@ -1,4 +1,13 @@
 package repository;
 
-public class IRepo {
+import java.util.Optional;
+import java.util.List;
+import java.util.UUID;
+
+public interface IRepo<T> {
+    void create(T obj);
+    Optional<T> read(UUID id);
+    void update(T obj);
+    void delete(UUID id);
+    List<T> findAll();
 }

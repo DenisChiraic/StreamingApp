aa8fe779692b9f3c32c2bfba225caf96470bc1dd
Denis <denis.chiriac@stud.ubbcluj.ro>
2024-11-09 13:03:42 +0200

    UserController has been added, logout and addToWatchList still need to be implemented

--- src/controller/UserController.java
+++ src/controller/UserController.java
@@ -1,4 +1,52 @@
 package controller;
 
+import model.*;
+import service.UserService;
+import service.ContentService;
+
+import java.util.Objects;
+import java.util.Optional;
+
 public class UserController {
+    private UserService userService;
+    private ContentService contentService;
+    private User currentUser;
+    private WatchList watchList;
+    private HistoryList historyList;
+
+    public UserController(UserService userService, ContentService contentService) {
+        this.userService = userService;
+        this.contentService = contentService;
+        this.watchList = new WatchList();
+        this.historyList = new HistoryList();
+    }
+
+    public boolean login(String username, String password) {
+        Optional<User> user = userService.authenticateUser(username, password);
+        if (user.isPresent()) {
+            currentUser = user.get();
+            System.out.println("Login successful");
+            return true;
+        } else {
+            System.out.println("Login failed");
+            return false;
+        }
+    }
+
+    public boolean logout() {
+       return false;
+    }
+
+    public void addToWatchList(Object content) {
+
+    }
+
+    public void displayWatchList() {
+        watchList.displayWatchList();
+    }
+
+    public User getCurrentUser() {
+        return currentUser;
+    }
+
 }
--- src/service/UserService.java
+++ src/service/UserService.java
@@ -1,4 +1,20 @@
 package service;
 
+import model.User;
+import repository.IRepo;
+
+import java.util.Optional;
+
 public class UserService {
+    private final IRepo<User> userRepo;
+
+    public UserService(IRepo<User> userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    public Optional<User> authenticateUser(String username, String password) {
+        return userRepo.findAll().stream()
+                .filter(user -> user.getUsername().equals(username) && user.getPassword().equals(password))
+                .findFirst();
+    }
 }

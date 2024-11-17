04b77bfa0cd5917f707be5293fe610ad843c4015
14ShaDoW141 <149473066+14ShaDoW141@users.noreply.github.com>
2024-11-17 17:31:38 +0200

    Added movies and serials cvs and the topList Class

--- src/model/TopList.java
+++ src/model/TopList.java
@@ -1,21 +1,37 @@
 package model;
 
 import java.util.List;
+import java.util.Comparator;
+import java.util.stream.Collectors;
+
+/**
+ * Clasa TopList gestioneaza un top 10 de filme si seriale bazate pe raiting
+ */
+
 
 public class TopList {
     private List<Movie> topMovies;
     private List<Serial> topSerials;
 
-    public TopList(List<Movie> topMovies, List<Serial> topSerials) {
-        this.topMovies = topMovies;
-        this.topSerials = topSerials;
+    public TopList() {}
+
+    public void updateTopMovies(List<Movie> movies) {
+        this.topMovies = movies.stream().sorted(Comparator.comparingDouble(Movie::getRating).reversed()).limit(10)
+                .collect(Collectors.toList());
     }
 
-    public List<Movie> getTopMovies() {
-        return topMovies;
+    public void updateTopSerials(List<Serial> serials) {
+        this.topSerials = serials.stream().sorted(Comparator.comparingDouble(Serial::getRating).reversed()).limit(10)
+                .collect(Collectors.toList());
     }
 
-    public List<Serial> getTopSerials() {
-        return topSerials;
+    public void displayTopMovies() {
+        System.out.println("Top 10 movies:");
+        topMovies.forEach(movie -> System.out.println("- " + movie.getTitle() + "(Rating: " + movie.getRating() + ")"));
+    }
+
+    public void displayTopSerials() {
+        System.out.println("Top 10 serials:");
+        topSerials.forEach(serial -> System.out.println("- " + serial.getTitle() + "(Rating: " + serial.getRating() + ")"));
     }
 }
